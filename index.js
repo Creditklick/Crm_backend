88f77c0c2edb4ad43edbbd2d84fb445d1c152de1
Creditklick <127750700+Creditklick@users.noreply.github.com>
2025-05-05 15:27:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -39,7 +39,8 @@ app.set("trust proxy",true);
 
 
 app.use(cors({
-    origin: 'https://crm-frontend-gyc8.vercel.app/',
+    origin: 'https://crm-frontend-gyc8.vercel.app',
+
     methods: ['GET', 'POST', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
